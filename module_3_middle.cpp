0d7387467d2e8bfd3db581204fa9266e2b20bffc
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-24 14:45:35 +0300

    Update module_3_middle.cpp

--- module_3_middle.cpp
+++ module_3_middle.cpp
@@ -1,1 +1,52 @@
-# middle
+long long itc_bin_num(long long number) {
+    long long binary = 0;
+    long long multiplier = 1;
+
+    while (number != 0) {
+        int digit = number % 2;
+        binary += digit * multiplier;
+        multiplier *= 10;
+        number /= 2;
+    }
+
+    return binary;
+}
+long long itc_oct_num(long long number){
+    long long binary = 0;
+    long long multiplier = 1;
+
+    while (number != 0) {
+        int digit = number % 8;
+        binary += digit * multiplier;
+        multiplier *= 10;
+        number /= 8;
+    }
+
+    return binary;
+}
+int itc_rev_bin_num(long long number) {
+    int dec = 0;
+    int multiplier = 1;
+
+    while (number != 0) {
+        int digit = number % 10;
+        dec += digit * multiplier;
+        multiplier *= 2;
+        number /= 10;
+    }
+
+    return dec;
+}
+int itc_rev_oct_num(long long number){
+    int dec = 0;
+    int multiplier = 1;
+
+    while (number != 0) {
+        int digit = number % 10;
+        dec += digit * multiplier;
+        multiplier *= 8;
+        number /= 10;
+    }
+
+    return dec;
+}
