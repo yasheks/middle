667051b9dd8cd1214e6e0b0680927daf62c370dd
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-24 14:46:25 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -1,1 +1,21 @@
-id# middle
+#ifndef MIDDLE_H_INCLUDED
+#define MIDDLE_H_INCLUDED
+void itc_num_print(int number);
+int itc_len_num(long long number);
+int itc_sum_num(long long number);
+long long itc_multi_num(long long number);
+int itc_max_num(long long number);
+int itc_min_num(long long number);
+int itc_rev_num(long long number);
+int itc_null_count(long long number);
+bool itc_mirror_num(long long number);
+int itc_mirror_count(long long number);
+int itc_second_max_num(long long number);
+int itc_second_simple_max_num(long long number);
+long long itc_bin_num(long long number);
+long long itc_oct_num(long long number);
+int itc_rev_bin_num(long long number);
+int itc_rev_oct_num(long long number);
+int itc_covert_num(long long number, int ss);
+int itc_rev_covert_num(long long number, int ss);
+#endif // MIDDLE_H_INCLUDED
