3ac29085adb5bd976f1a9ee24a595bdac5c7d91c
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-24 14:45:56 +0300

    Update module_4_middle.cpp

--- module_4_middle.cpp
+++ module_4_middle.cpp
@@ -1,1 +1,29 @@
-# middle
+int itc_covert_num(long long number, int ss)
+{
+    long long system = 0;
+    long long multiplier = 1;
+
+    while (number != 0) {
+        int digit = number % ss;
+        system += digit * multiplier;
+        multiplier *= 10;
+        number /= ss;
+    }
+
+    return system;
+}
+int itc_rev_covert_num(long long number, int ss)
+{
+    int dec = 0;
+    int multiplier = 1;
+
+    while (number != 0) {
+        int digit = number % 10;
+        dec += digit * multiplier;
+        multiplier *= ss;
+        number /= 10;
+    }
+
+    return dec;
+}
+
