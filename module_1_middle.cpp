bdeb3fa1f809dc0bf10e41414e85496c8d601498
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-26 19:44:43 +0300

    Update module_1_middle.cpp

--- module_1_middle.cpp
+++ module_1_middle.cpp
@@ -9,6 +9,8 @@ void itc_num_print(int number)
 int itc_len_num(long long num)
 {
     int razrads = 0;
+    if (num < 0)
+        num = -num;
     if (num == 0)
         return 1;
     while (num!=0)
