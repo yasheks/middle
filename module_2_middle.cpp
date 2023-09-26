986cc023ed04868eb46e14595592720257f6d1b0
yasheks <143996832+yasheks@users.noreply.github.com>
2023-09-26 21:51:00 +0300

    Update module_2_middle.cpp

--- module_2_middle.cpp
+++ module_2_middle.cpp
@@ -71,6 +71,10 @@ bool itc_mirror_num(long long number)
 int itc_mirror_count(long long number)
 {
     int col = 0;
+    if (number <0)
+        number = -number
+    if (number == 0)
+        return 1;
     for (int i = 0; i<number; i++)
     {
         if (itc_mirror_num(i) == 1)
